e0bf5c858fb679d67df5dbfd57fcbd2a284b7322
rohmanalka <rohmanalka06@gmail.com>
2024-02-25 07:47:30 +0700

    First Main

---
+++ BukuMain21.java
@@ -0,0 +1,17 @@
+public class BukuMain21 {
+    public static void main(String[] args) {
+        Buku21 bk1 = new Buku21();
+        bk1.judul = "Today Ends Tomorrow Comes";
+        bk1.pengarang = "Denanda Pratiwi";
+        bk1.halaman = 198;
+        bk1.stok = 13;
+        bk1.harga = 71000;
+
+        int jml, hrg;
+
+        bk1.tampilInformasi();
+        bk1.terjual(jml=5);
+        bk1.gantiHarga(hrg=60000);
+        bk1.tampilInformasi();
+    }
+}
