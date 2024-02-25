5dca89351306feb9ab73cb2ca086f4843e7591e0
rohmanalka <rohmanalka06@gmail.com>
2024-02-25 07:49:36 +0700

    penambahan konstruktor

---
+++ Buku21.java
@@ -0,0 +1,36 @@
+public class Buku21 {
+    String judul, pengarang;
+    int halaman, stok, harga;
+
+    void tampilInformasi() {
+        System.out.println("Judul: " + judul);
+        System.out.println("Pengarang: " + pengarang);
+        System.out.println("Jumlah Halaman: " + halaman);
+        System.out.println("Sisa Stok: " + stok);
+        System.out.println("Harga: Rp " + harga);
+    }
+
+    void terjual(int jml) {
+        stok -= jml;
+    }
+
+    void restock(int jml) {
+        stok += jml;
+    }
+
+    void gantiHarga(int hrg) {
+        harga = hrg;
+    }
+
+    public Buku21() {
+
+    }
+    
+    public Buku21(String jdl, String pg, int hal, int stok, int hrg) {
+        judul = jdl;
+        pengarang = pg;
+        halaman = hal;
+        this.stok = stok;
+        harga = hrg;
+    }
+}
