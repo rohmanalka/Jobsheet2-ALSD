cb8ad4ee8667736364ddfc45b3b43bb4f9bc0400
rohmanalka <rohmanalka06@gmail.com>
2024-02-28 14:35:06 +0700

    First

---
+++ DragonMain21.java
@@ -0,0 +1,12 @@
+public class DragonMain21 {
+    public static void main(String[] args) {
+        Dragon21 dragon = new Dragon21(0, 0, 5, 5);
+        dragon.moveRight();
+        dragon.moveRight();
+        dragon.moveRight();
+        dragon.moveRight();
+        dragon.moveRight();
+        dragon.moveRight();
+        dragon.printPosition();
+    }
+}
